e19e11c53e335cef789ba548dc15da593325fcbd
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2023-08-23 16:52:37 -0600

    L1B Done

--- Set1/L1B/main.cpp
+++ Set1/L1B/main.cpp
@@ -1,8 +1,8 @@
 /* CSCI 200: Lab 1B: Random Classification
  *
  * Author: Kyle Kubaska
- * 
-*/
+ *
+ */
 
 #include <iostream>
 #include <random>
@@ -10,8 +10,44 @@
 
 using namespace std;
 
-int main() {
+float random(const float min, const float max)
+{
+    return min + ((float)rand() / (float)RAND_MAX) * (max - min);
+}
+
+int main()
+{
+    srand(time(NULL));
     float min, max;
-    
+    char userIn;
+    do
+    {
+        cout << "Enter the minimum value: ";
+        cin >> min;
+        cout << "Enter the maximum value: ";
+        cin >> max;
+        float randomNumber = random(min, max);
+        cout << "A random number is: " << randomNumber << endl;
+        float percentile = randomNumber / (max - min);
+        if (percentile >= .75)
+        {
+            cout << "This is in the fourth quartile." << endl;
+        }
+        else if (percentile >= .5)
+        {
+            cout << "This is in the third quartile." << endl;
+        }
+        else if (percentile >= .25)
+        {
+            cout << "This is in the second quartile." << endl;
+        }
+        else
+        {
+            cout << "This is in the first quartile." << endl;
+        }
+        cout << "Do you want another random value? (Y/N) ";
+        cin >> userIn;
+    } while (userIn == 'Y');
+    cout << "Have a nice day!";
     return 0;
 }
