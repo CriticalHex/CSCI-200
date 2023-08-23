69608e08d91464cdfb00df1447264e8806747180
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2023-08-23 08:55:52 -0600

    Add files via upload

---
+++ Set1/L1A/main.cpp
@@ -0,0 +1,46 @@
+/* CSCI 200: Lab 1A: Math Equation Solver
+ *
+ * Author: Kyle Kubaska
+ *
+ * More complete description here...
+ */
+
+// The include section adds extra definitions from the C++ standard library.
+#include <iostream> // For cin, cout, etc.
+#include <cmath>
+
+// We will (most of the time) use the standard library namespace in our programs.
+using namespace std;
+
+// Define any constants below this comment.
+float ohmsLaw(const float voltage, const float resistance) {
+    return voltage / resistance; //current
+}
+
+float distance(const float x, const float y) {
+    return sqrt(pow(x,2.0f) + pow(x,2.0f));
+}
+
+string promptFor(const string prompt) {
+    string userIn;
+    cout << prompt;
+    cin >> userIn;
+    return userIn;
+}
+
+// Must have a function named "main", which is the starting point of a C++ program.
+int main() {
+
+  /******** INSERT YOUR CODE BELOW HERE ********/
+    float x, y, voltage, resistance;
+    voltage = stof(promptFor("Enter the voltage: "));
+    resistance = stof(promptFor("Enter the resistance: "));
+    cout << "The current is: " << ohmsLaw(voltage, resistance) << endl;
+    x = stof(promptFor("Enter the x: "));
+    y = stof(promptFor("Enter the y: "));
+    cout << "The distance is: " << distance(x, y) << endl;
+
+  /******** INSERT YOUR CODE ABOVE HERE ********/
+
+  return 0; // signals the operating system that our program ended OK.
+}
---
+++ Set1/L1B/main.cpp
@@ -0,0 +1,17 @@
+/* CSCI 200: Lab 1B: Random Classification
+ *
+ * Author: Kyle Kubaska
+ * 
+*/
+
+#include <iostream>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    float min, max;
+    
+    return 0;
+}
