8a5ce39059bbbd8a046a1479d3a58565160cf1f3
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2023-09-05 00:14:08 -0600

    Formatting Changes

--- Set1/A1/main.cpp
+++ Set1/A1/main.cpp
@@ -1,21 +1,24 @@
 /* CSCI 200: Assignment 1: A1 - Rock Paper Scissor Throw Down!
  *
  * Author: Kyle Kubaska
- * Resources used (Office Hours, Tutoring, Other Students, etc & in what capacity):
+ * Resources used (Office Hours, Tutoring, Other Students, etc & in what
+ * capacity):
  *     // list here any outside assistance you used/received while following the
  *     // CS@Mines Collaboration Policy and the Mines Academic Code of Honor
  *
  * XXXXXXXX (MORE_COMPLETE_DESCRIPTION_HERE)
  */
 // The include section adds extra definitions from the C++ standard library.
-#include <iostream> // For cin, cout, etc.
-#include <random>
 #include <ctime>
+#include <iostream> // For cin, cout, etc.
 #include <map>
+#include <random>
 
-// We will (most of the time) use the standard library namespace in our programs.
+// We will (most of the time) use the standard library namespace in our
+// programs.
 using namespace std;
-// using std::cout; // was telling me cout was ambiguous despite no compiler errors;
+// using std::cout; // was telling me cout was ambiguous despite no compiler
+// errors;
 
 // Define any constants below this comment.
 const char computerChoices[] = {'R', 'P', 'S'};
@@ -23,94 +26,77 @@ const char computerChoices[] = {'R', 'P', 'S'};
 char promptUser();
 char choose();
 
-void pWin()
-{
-    cout << "Player wins!" << endl;
+void pWin() { cout << "Player wins!" << endl; }
+void cWin() { cout << "Computer wins!" << endl; }
+void tiesWith(string played) {
+  cout << played << " ties with " << played << "." << endl;
+  cout << "It's a tie!" << endl;
 }
-void cWin()
-{
-    cout << "Computer wins!" << endl;
-}
-void tiesWith(string played)
-{
-    cout << played << " ties with " << played << "." << endl;
-    cout << "It's a tie!" << endl;
-}
-void thisBeatsThat(string winner, string loser)
-{
-    cout << winner << " beats " << loser << "." << endl;
+void thisBeatsThat(string winner, string loser) {
+  cout << winner << " beats " << loser << "." << endl;
 }
 
-// Must have a function named "main", which is the starting point of a C++ program.
-int main()
-{
+// Must have a function named "main", which is the starting point of a C++
+// program.
+int main() {
 
-    /******** INSERT YOUR CODE BELOW HERE ********/
-    srand(time(NULL));
+  /******** INSERT YOUR CODE BELOW HERE ********/
+  srand(time(NULL));
 
-    // cout << "Hello world!" << endl; // print Hello world! to the screen
+  // cout << "Hello world!" << endl; // print Hello world! to the screen
 
-    char userIn, computerChoice;
-    int won = 0, lost = 0, tied = 0;
-    map<char, char> beats = {{'R', 'S'}, {'P', 'R'}, {'S', 'P'}};
-    map<char, string> getWord = {{'R', "Rock"}, {'P', "Paper"}, {'S', "Scissors"}};
+  char userIn, computerChoice;
+  int won = 0, lost = 0, tied = 0;
+  map<char, char> beats = {{'R', 'S'}, {'P', 'R'}, {'S', 'P'}};
+  map<char, string> getWord = {
+      {'R', "Rock"}, {'P', "Paper"}, {'S', "Scissors"}};
 
-    do
-    {
-        userIn = promptUser();
-        computerChoice = choose();
-        cout << "Player choose " << getWord[userIn] << endl;
-        cout << "Computer choose " << getWord[computerChoice] << endl
-             << endl;
+  do {
+    userIn = promptUser();
+    computerChoice = choose();
+    cout << "Player choose " << getWord[userIn] << endl;
+    cout << "Computer choose " << getWord[computerChoice] << endl << endl;
 
-        if (userIn == computerChoice)
-        {
-            tiesWith(getWord[userIn]);
-            tied++;
-        }
-        else if (beats[userIn] == computerChoice)
-        {
-            thisBeatsThat(getWord[userIn], getWord[computerChoice]);
-            pWin();
-            won++;
-        }
-        else
-        {
-            thisBeatsThat(getWord[computerChoice], getWord[userIn]);
-            cWin();
-            lost++;
-        }
+    if (userIn == computerChoice) {
+      tiesWith(getWord[userIn]);
+      tied++;
+    } else if (beats[userIn] == computerChoice) {
+      thisBeatsThat(getWord[userIn], getWord[computerChoice]);
+      pWin();
+      won++;
+    } else {
+      thisBeatsThat(getWord[computerChoice], getWord[userIn]);
+      cWin();
+      lost++;
+    }
 
-        cout << endl
-             << "Do you want to play again? (Y/N) ";
-        cin >> userIn;
-        cout << endl;
-
-    } while (userIn == 'Y');
-
-    cout << "Thanks for playing!" << endl
-         << "You won " << won
-         << " game(s), lost " << lost
-         << " game(s), and tied " << tied
-         << " game(s).";
-
-    /******** INSERT YOUR CODE ABOVE HERE ********/
-
-    return 0; // signals the operating system that our program ended OK.
-}
-
-char promptUser()
-{
-    char userIn;
-    cout << "Welcome one and all to a round of Rock, Paper, Scissors! (Enter P, R or S)" << endl;
-    cout << "Player one: ";
+    cout << endl << "Do you want to play again? (Y/N) ";
     cin >> userIn;
     cout << endl;
-    return userIn;
+
+  } while (userIn == 'Y');
+
+  cout << "Thanks for playing!" << endl
+       << "You won " << won << " game(s), lost " << lost
+       << " game(s), and tied " << tied << " game(s).";
+
+  /******** INSERT YOUR CODE ABOVE HERE ********/
+
+  return 0; // signals the operating system that our program ended OK.
 }
 
-char choose()
-{
-    const int random = rand() % 3;
-    return computerChoices[random];
+char promptUser() {
+  char userIn;
+  cout << "Welcome one and all to a round of Rock, Paper, Scissors! (Enter P, "
+          "R or S)"
+       << endl;
+  cout << "Player one: ";
+  cin >> userIn;
+  cout << endl;
+  return userIn;
+}
+
+char choose() {
+  const int random = rand() % 3;
+  return computerChoices[random];
 }
--- Set1/L1B/main.cpp
+++ Set1/L1B/main.cpp
@@ -4,50 +4,40 @@
  *
  */
 
+#include <ctime>
 #include <iostream>
 #include <random>
-#include <ctime>
 
 using namespace std;
 
-float random(const float min, const float max)
-{
-    return min + ((float)rand() / (float)RAND_MAX) * (max - min);
+float random(const float min, const float max) {
+  return min + ((float)rand() / (float)RAND_MAX) * (max - min);
 }
 
-int main()
-{
-    srand(time(NULL));
-    float min, max;
-    char userIn;
-    do
-    {
-        cout << "Enter the minimum value: ";
-        cin >> min;
-        cout << "Enter the maximum value: ";
-        cin >> max;
-        float randomNumber = random(min, max);
-        cout << "A random number is: " << randomNumber << endl;
-        float percentile = randomNumber / (max - min);
-        if (percentile >= .75)
-        {
-            cout << "This is in the fourth quartile." << endl;
-        }
-        else if (percentile >= .5)
-        {
-            cout << "This is in the third quartile." << endl;
-        }
-        else if (percentile >= .25)
-        {
-            cout << "This is in the second quartile." << endl;
-        }
-        else
-        {
-            cout << "This is in the first quartile." << endl;
-        }
-        cout << "Do you want another random value? (Y/N) ";
-        cin >> userIn;
-    } while (userIn == 'Y');
-    cout << "Have a nice day!";
-    return 0;
+int main() {
+  srand(time(NULL));
+  float min, max;
+  char userIn;
+  do {
+    cout << "Enter the minimum value: ";
+    cin >> min;
+    cout << "Enter the maximum value: ";
+    cin >> max;
+    float randomNumber = random(min, max);
+    cout << "A random number is: " << randomNumber << endl;
+    float percentile = randomNumber / (max - min);
+    if (percentile >= .75) {
+      cout << "This is in the fourth quartile." << endl;
+    } else if (percentile >= .5) {
+      cout << "This is in the third quartile." << endl;
+    } else if (percentile >= .25) {
+      cout << "This is in the second quartile." << endl;
+    } else {
+      cout << "This is in the first quartile." << endl;
+    }
+    cout << "Do you want another random value? (Y/N) ";
+    cin >> userIn;
+  } while (userIn == 'Y');
+  cout << "Have a nice day!";
+  return 0;
 }
